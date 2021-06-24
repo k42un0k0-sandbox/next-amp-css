823ab29c5f953c17e73317f8f34988726ba8e674
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-24 15:36:55 +0900

    ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { CacheProvider } from '@emotion/react';
+import { cache } from '../lib/emotion';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+type Props = {
+  Component: React.VFC;
+  pageProps: Record<string, unknown>;
+};
+
+
+function MyApp({ Component, pageProps }: Props): JSX.Element {
+  return (
+    <CacheProvider value={cache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+/** @jsxImportSource react */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { extractCritical } from '../lib/emotion';
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    const page = await ctx.renderPage();
+    const emo = extractCritical(page.html);
+    const styles = [
+      //@ts-expect-error aaa
+      ...initialProps.styles,
+      <style key="custom" data-emotion-css={emo.ids.join(' ')} dangerouslySetInnerHTML={{ __html: emo.css }} />,
+    ];
+    return { ...initialProps, styles };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
