f8a3f730a41a5cb4f14462165cd7db7d5ce83f5e
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-24 15:36:58 +0900

    media query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import { Interpolation, Theme } from '@emotion/react'
+
+import facepaint from 'facepaint'
+
+const breakpoints = [576, 768, 992, 1200]
+
+const mq = facepaint(
+  breakpoints.map(bp => `@media (min-width: ${bp}px)`)
+)
+
+const styles: Record<string, Interpolation<Theme>> = {
+  container: mq({
+    display: 'flex',
+    flexWrap: 'wrap'
+  }),
+  content: mq({
+    flex: '0 0 auto',
+    width: ['50%', 'calc(100% / 3)', '25%']
+  })
+
+}
+
 export const config = { amp: true }
 
 export default function Home() {
   return (
-    <div></div>
+    <div css={styles.container}>{
+      [...Array(10).keys()].map((i) => {
+        return <div key={i} css={styles.content}>{i}</div>
+      })
+    }</div>
   )
 }
